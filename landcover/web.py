0d2614997eaef7333439bf9c28e7ef4a84cf5f49
Seth Fitzsimmons <seth@mojodna.net>
2018-09-21 16:34:40 -0700

    Use PostGISCatalog, colormaps, and clean up

--- landcover/web.py
+++ landcover/web.py
@@ -3,33 +3,50 @@ from __future__ import absolute_import
 
 import logging
 from urllib.parse import urlencode
+from logging import StreamHandler
 
 from cachetools.func import lru_cache
 from flask import Markup, jsonify, render_template, request, url_for
 from marblecutter import NoCatalogAvailable, tiling
+from marblecutter.catalogs.postgis import PostGISCatalog
+from marblecutter.formats.geotiff import GeoTIFF
 from marblecutter.formats.png import PNG
-from marblecutter.transformations import Image
+from marblecutter.transformations import Colormap
 from marblecutter.web import app
 from mercantile import Tile
 
-from .catalogs import VirtualCatalog
+nothing = 0
+water = 10
+developed = 20
+barren = 30
+forest = 40
+shrubland = 50
+herbaceous = 70
+cultivated = 80
+wetlands = 90
+
+COLORMAP = {
+    water: (69, 128, 162),
+    forest: (27, 119, 28),
+    shrubland: (173, 164, 127),
+    herbaceous: (188, 212, 149),
+    wetlands: (146, 184, 186),
+    cultivated: (215, 214, 114),
+    developed: (203, 8, 20),
+    barren: (198, 180, 134),
+}
 
 LOG = logging.getLogger(__name__)
-
-IMAGE_TRANSFORMATION = Image()
+CATALOG = PostGISCatalog(table="land_cover")
+COLORMAP_TRANSFORMATION = Colormap(COLORMAP)
 IMAGE_FORMAT = PNG()
+GEOTIFF_FORMAT = GeoTIFF(colormap=COLORMAP)
 
+# configure logging
 
-@lru_cache()
-def make_catalog(args):
-    try:
-        return VirtualCatalog(
-            args["url"],
-            resample=args.get("resample"),
-        )
-    except Exception as e:
-        LOG.warn(e)
-        raise NoCatalogAvailable()
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("rasterio._base").setLevel(logging.WARNING)
+logging.getLogger("botocore.credentials").setLevel(logging.WARNING)
 
 
 def make_prefix():
@@ -40,53 +57,32 @@ def make_prefix():
         return request.headers.get("X-Stage")
 
 
-@app.route("/")
-@app.route("/<prefix>/")
-def index(prefix=None):
-    return "marblecutter-land-cover"
-
-
 @app.route("/tiles/")
 @app.route("/<prefix>/tiles/")
 def meta(prefix=None):
-    catalog = make_catalog(request.args)
-
     meta = {
-        "bounds": catalog.bounds,
-        "center": catalog.center,
-        "maxzoom": catalog.maxzoom,
-        "minzoom": catalog.minzoom,
-        "name": catalog.name,
+        "bounds": CATALOG.bounds,
+        "center": CATALOG.center,
+        "maxzoom": CATALOG.maxzoom,
+        "minzoom": CATALOG.minzoom,
+        "name": CATALOG.name,
         "tilejson": "2.2.0",
-    }
-
-    with app.app_context():
-        meta["tiles"] = [
+        "tiles": [
             "{}{{z}}/{{x}}/{{y}}?{}".format(
                 url_for("meta", prefix=make_prefix(), _external=True, _scheme=""),
                 urlencode(request.args),
             )
-        ]
+        ],
+    }
 
     return jsonify(meta)
-
-
-@app.route("/bounds/")
-@app.route("/<prefix>/bounds/")
-def bounds(prefix=None):
-    catalog = make_catalog(request.args)
-
-    return jsonify({"url": catalog.uri, "bounds": catalog.bounds})
 
 
 @app.route("/preview")
 @app.route("/<prefix>/preview")
 def preview(prefix=None):
-    # initialize the catalog so this route will fail if the source doesn't exist
-    make_catalog(request.args)
-
-    with app.app_context():
-        return render_template(
+    return (
+        render_template(
             "preview.html",
             tilejson_url=Markup(
                 url_for(
@@ -97,27 +93,39 @@ def preview(prefix=None):
                     **request.args
                 )
             ),
-        ), 200, {
-            "Content-Type": "text/html"
-        }
+        ),
+        200,
+        {"Content-Type": "text/html"},
+    )
 
 
 @app.route("/tiles/<int:z>/<int:x>/<int:y>")
 @app.route("/tiles/<int:z>/<int:x>/<int:y>@<int:scale>x")
-@app.route("/<prefix>/tiles/<image_id>/<int:z>/<int:x>/<int:y>")
 @app.route("/<prefix>/tiles/<int:z>/<int:x>/<int:y>")
+@app.route("/<prefix>/tiles/<int:z>/<int:x>/<int:y>@<int:scale>x")
 def render_png(z, x, y, scale=1, prefix=None):
-    catalog = make_catalog(request.args)
     tile = Tile(x, y, z)
 
     headers, data = tiling.render_tile(
         tile,
-        catalog,
+        CATALOG,
         format=IMAGE_FORMAT,
-        transformation=IMAGE_TRANSFORMATION,
+        transformation=COLORMAP_TRANSFORMATION,
         scale=scale,
     )
 
-    headers.update(catalog.headers)
+    headers.update(CATALOG.headers)
+
+    return data, 200, headers
+
+
+@app.route("/tiles/<int:z>/<int:x>/<int:y>.tif")
+@app.route("/<prefix>/tiles/<int:z>/<int:x>/<int:y>.tif")
+def render_tif(z, x, y, prefix=None):
+    tile = Tile(x, y, z)
+
+    headers, data = tiling.render_tile(tile, CATALOG, format=GEOTIFF_FORMAT)
+
+    headers.update(CATALOG.headers)
 
     return data, 200, headers
